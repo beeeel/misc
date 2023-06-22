f67af09905a008812541f1b9f59bcb9d2c5cc576
beeeel <50520111+beeeel@users.noreply.github.com>
2023-06-23 00:36:04 +0100

    Wrote a basic interrupt-based flow sensor control

---
+++ flow_sensor_v1/flow_sensor_v1.ino
@@ -0,0 +1,62 @@
+int tLast, tNow;
+bool flowState;
+int motorPin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // Say hi!
+  Serial.begin(9600);
+  Serial.println(F("hi!"));
+  
+  // Does what it says
+  attachInterrupt(digitalPinToInterrupt(2),inputChange,CHANGE); 
+
+  // Prepare the output pin
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Got here!");
+
+  while ( !flowState )
+  {
+    // Do nothing until a flow is detected
+  }
+
+  // Flow has been detected so turn on the output pin
+  analogWrite(motorPin, HIGH);
+  Serial.println("Turned on motor");
+  
+  while ( flowState )
+  {
+    Serial.println("Haven't turned it off yet");
+    // Now check in case the flow has stopped
+    delay(500);
+    tNow = millis();
+    // If the last event is more than 500ms ago turn off
+    if ( tNow > ( tLast + 500 ) )
+    {
+      flowState = false;
+    }
+  }
+
+  // Turn off the motor
+  analogWrite(motorPin, LOW);
+  Serial.println("Turned off motor");
+}
+
+void inputChange()
+{
+  tNow = millis();
+  // if the last event is more than 1000ms ago do nothing
+  if ( tNow > ( tLast + 1000 ) )
+  {
+    tLast = tNow;
+  }
+  else
+  {
+    flowState = true;
+  }
+}
