d3199f8d20861d3f2466a12d8be458fb345a37c2
beeeel <50520111+beeeel@users.noreply.github.com>
2023-06-28 11:18:18 +0100

    Moved stuff to header

--- flow_sensor_v1/flow_sensor_v1.h
+++ flow_sensor_v1/flow_sensor_v1.h
@@ -1,1 +1,32 @@
 // Testvoid 
+
+volatile int tLast, tNow;
+volatile bool flowState;
+int motorPin = 10;
+
+void setupFlowSensor()
+{
+  // Say hi!
+  Serial.begin(9600);
+  Serial.println(F("Hi from flow sensor!"));
+  
+  // Does what it says
+  attachInterrupt(digitalPinToInterrupt(2),inputChange,CHANGE); 
+
+  // Prepare the output pin
+  pinMode(motorPin, OUTPUT);
+}
+
+void inputChange()
+{
+  tNow = millis();
+  // if the last event is more than 1000ms ago do nothing
+  if ( tNow > ( tLast + 1000 ) )
+  {
+    tLast = tNow;
+  }
+  else
+  {
+    flowState = true;
+  }
+}
--- flow_sensor_v1/flow_sensor_v1.ino
+++ flow_sensor_v1/flow_sensor_v1.ino
@@ -1,19 +1,9 @@
-volatile int tLast, tNow;
-volatile bool flowState;
-int motorPin = 10;
+
 
 void setup() {
   // put your setup code here, to run once:
 
-  // Say hi!
-  Serial.begin(9600);
-  Serial.println(F("hi!"));
-  
-  // Does what it says
-  attachInterrupt(digitalPinToInterrupt(2),inputChange,CHANGE); 
-
-  // Prepare the output pin
-  pinMode(motorPin, OUTPUT);
+  setupFlowSensor();
 }
 
 void loop() {
@@ -45,18 +35,4 @@ void loop() {
   // Turn off the motor
   analogWrite(motorPin, LOW);
   Serial.println("Turned off motor");
-}
-
-void inputChange()
-{
-  tNow = millis();
-  // if the last event is more than 1000ms ago do nothing
-  if ( tNow > ( tLast + 1000 ) )
-  {
-    tLast = tNow;
-  }
-  else
-  {
-    flowState = true;
-  }
 }
